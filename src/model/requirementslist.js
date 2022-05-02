3988cdedb83ec6a5ed568c643efeacf5e6ae1d06
Bhagitha paramesh <bhagithaparamesh@gmail.com>
2022-05-02 13:57:48 +0530

    Initial commit

---
+++ src/model/requirementslist.js
@@ -0,0 +1,10 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema    //schema definition
+
+const RequirementslistSchema = new Schema({
+    login_id: { type: Schema.Types.ObjectId, ref: "login_tb", required: true },
+    requirements_list:{type:Array}
+})
+
+var RequirementsData = mongoose.model('requirementslist_tb', RequirementslistSchema) //model creation
+module.exports = RequirementsData;
---
+++ src/model/useradonservices.js
@@ -0,0 +1,11 @@
+const mongoose = require('mongoose')
+const Schema = mongoose.Schema    //schema definition
+
+const UserAdonSchema = new Schema({
+    login_id: { type: Schema.Types.ObjectId, ref: "login_tb", required: true },
+    adonservice_name:[{String}],
+    total_amount:{type:Number}
+})
+
+var userAdonData = mongoose.model('useradon_tb', UserAdonSchema) //model creation
+module.exports = userAdonData;
